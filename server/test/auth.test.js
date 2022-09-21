992fa8c0164d02bd4bf458041268e1c58147c37f
Mir1001 <matej.crepinsek@gmail.com>
2022-09-21 08:47:06 +0200

    run test in sequence, use async instead of promiss

--- server/test/auth.test.js
+++ server/test/auth.test.js
@@ -30,160 +30,134 @@ describe('auth endpoints', () => {
   };
 
   beforeEach(async () => {
+    //clearDb();
+    await mongoose.connection.dropDatabase();
     user = validUser();
-    console.log('--------------------------------');
-    console.log(user);
-    console.log('--------------------------------');
     const hashedPassword = await hashPassword(user.password);
     await new User({ ...user, password: hashedPassword }).save();
   });
 
   describe('/authenticate', () => {
-    it('rejects requests with no credentials', (done) => {
-      request(app)
-        .post('/api/authenticate')
-        .expect((res) => {
-          expect(res.body.message).toBeDefined();
-          expect(res.body.message).toContain('required');
-        })
-        .expect(422, done);
+    it('rejects requests with no credentials', async () => {
+      const res = await request(app).post('/api/authenticate');
+      expect(res.body.message).toBeDefined();
+      expect(res.body.message).toContain('required');
+      expect(res.statusCode).toEqual(422);
     });
 
-    it('reject requests with incorrect email', (done) => {
-      request(app)
+    it('reject requests with incorrect email', async () => {
+      const res = await request(app)
         .post('/api/authenticate')
-        .send({ ...user, email: email.nonExisting })
-        .expect((res) => {
-          expect(res.body.message).toContain('Wrong email or password.');
-        })
-        .expect(403, done);
+        .send({ ...user, email: email.nonExisting });
+      expect(res.body.message).toContain('Wrong email or password.');
+
+      expect(res.statusCode).toEqual(403);
     });
 
-    it('reject requests with incorrect password', (done) => {
-      request(app)
+    it('reject requests with incorrect password', async () => {
+      const res = await request(app)
         .post('/api/authenticate')
-        .send({ ...user, password: password.wrong })
-        .expect((res) => {
-          expect(res.body.message).toContain('Wrong email or password.');
-        })
-        .expect(403, done);
+        .send({ ...user, password: password.wrong });
+      expect(res.body.message).toContain('Wrong email or password.');
+
+      expect(res.statusCode).toEqual(403);
     });
 
-    it('returns a valid auth token', (done) => {
-      request(app)
-        .post('/api/authenticate')
-        .send(user)
-        .expect('Content-Type', /json/)
-        .expect((res) => {
-          const { token } = res.body;
-          const decodedToken = jwtDecode(token);
-          expect(decodedToken.email).toEqual(user.email);
-        })
-        .expect(200, done);
+    it('returns a valid auth token', async () => {
+      const res = await request(app).post('/api/authenticate').send(user);
+      //.expect('Content-Type', /json/)
+      const { token } = res.body;
+      const decodedToken = jwtDecode(token);
+      expect(decodedToken.email).toEqual(user.email);
+      expect(res.statusCode).toEqual(200);
     });
   });
 
   describe('/signup', () => {
-    it('rejects requests with missing fields', (done) => {
-      request(app)
-        .post('/api/signup')
-        .expect((res) => {
-          expect(res.body.message).toBeDefined();
-          expect(res.body.message).toContain('required');
-        })
-        .expect(422, done);
+    it('rejects requests with missing fields', async () => {
+      const res = await request(app).post('/api/signup');
+      expect(res.body.message).toBeDefined();
+      expect(res.body.message).toContain('required');
+
+      expect(res.statusCode).toEqual(422);
     });
 
-    it('rejects requests with blank name', (done) => {
-      request(app)
+    it('rejects requests with blank name', async () => {
+      const res = await request(app)
         .post('/api/signup')
-        .send({ ...user, email: email.blank })
-        .expect((res) => {
-          expect(res.body.message).toBeDefined();
-          expect(res.body.message).toContain('cannot be blank');
-        })
-        .expect(422, done);
+        .send({ ...user, email: email.blank });
+      expect(res.body.message).toBeDefined();
+      expect(res.body.message).toContain('cannot be blank');
+
+      expect(res.statusCode).toEqual(422);
     });
 
-    it('rejects requests with blank password', (done) => {
-      request(app)
+    it('rejects requests with blank password', async () => {
+      const res = await request(app)
         .post('/api/signup')
-        .send({ ...user, password: password.blank })
-        .expect((res) => {
-          expect(res.body.message).toBeDefined();
-          expect(res.body.message).toContain('cannot be blank');
-        })
-        .expect(422, done);
+        .send({ ...user, password: password.blank });
+      expect(res.body.message).toBeDefined();
+      expect(res.body.message).toContain('cannot be blank');
+
+      expect(res.statusCode).toEqual(422);
     });
 
-    it('rejects requests with invalid name', (done) => {
-      request(app)
+    it('rejects requests with invalid name', async () => {
+      const res = await request(app)
         .post('/api/signup')
-        .send({ ...user, email: email.invalid })
-        .expect((res) => {
-          expect(res.body.message).toBeDefined;
-          expect(res.body.message).toContain('wrong');
-        })
-        .expect(422, done);
+        .send({ ...user, email: email.invalid });
+      expect(res.body.message).toBeDefined;
+      expect(res.body.message).toContain('wrong');
+
+      expect(res.statusCode).toEqual(422);
     });
 
-    it('rejects request with password that is too long', (done) => {
-      request(app)
+    it('rejects request with password that is too long', async () => {
+      const res = await request(app)
         .post('/api/signup')
-        .send({ ...user, password: password.long })
-        .expect((res) => {
-          expect(res.body.message).toBeDefined();
-          expect(res.body.message).toContain('at most 50 characters long');
-        })
-        .expect(422, done);
+        .send({ ...user, password: password.long });
+      expect(res.body.message).toBeDefined();
+      expect(res.body.message).toContain('at most 50 characters long');
+
+      expect(res.statusCode).toEqual(422);
     });
 
-    it('rejects requests with password that is too short', (done) => {
-      request(app)
+    it('rejects requests with password that is too short', async () => {
+      const res = await request(app)
         .post('/api/signup')
-        .send({ ...user, password: password.short })
-        .expect((res) => {
-          expect(res.body.message).toBeDefined();
-          expect(res.body.message).toContain('at least 6 characters long');
-        })
-        .expect(422, done);
+        .send({ ...user, password: password.short });
+      expect(res.body.message).toBeDefined();
+      expect(res.body.message).toContain('at least 6 characters long');
+
+      expect(res.statusCode).toEqual(422);
     });
 
-    it('rejects requests with existing email', (done) => {
-      request(app)
+    it('rejects requests with existing email', async () => {
+      const res = await request(app)
         .post('/api/signup')
-        .send({ ...user, username: faker.name.firstName() })
-        .expect((res) => {
-          expect(res.body.message).toBeDefined();
-          expect(res.body.message).toContain('already');
-        })
-        .expect(422, done);
+        .send({ ...user, username: faker.name.firstName() });
+      expect(res.body.message).toBeDefined();
+      expect(res.body.message).toContain('already');
+      expect(res.statusCode).toEqual(422);
     });
-    it('rejects requests with existing username', (done) => {
-      request(app)
+    it('rejects requests with existing username', async () => {
+      const res = await request(app)
         .post('/api/signup')
-        .send({ ...user, email: faker.internet.email().toLowerCase() })
-        .expect((res) => {
-          expect(res.body.message).toBeDefined();
-          expect(res.body.message).toContain('already');
-        })
-        .expect(422, done);
+        .send({ ...user, email: faker.internet.email().toLowerCase() });
+      expect(res.body.message).toBeDefined();
+      expect(res.body.message).toContain('already');
+      expect(res.statusCode).toEqual(422);
     });
 
-    it('creates a new user and returns a valid auth token', (done) => {
+    it('creates a new user and returns a valid auth token', async () => {
       user.email = 'a' + user.email; //one user already in db
       user.username = 'a' + user.username;
-
-      request(app)
-        .post('/api/signup')
-        .send(user)
-        //  .expect('Content-Type', /json/)
-        .expect((res) => {
-          const { token } = res.body;
-          const decodedToken = jwtDecode(token);
-          expect(decodedToken.email.toLowerCase()).toEqual(user.email.toLowerCase());
-        })
-        .expect(201, done);
+      const res = await request(app).post('/api/signup').send(user);
+      //  .expect('Content-Type', /json/)
+      const { token } = res.body;
+      const decodedToken = jwtDecode(token);
+      expect(decodedToken.email.toLowerCase()).toEqual(user.email.toLowerCase());
+      expect(res.statusCode).toEqual(201);
     });
   });
 });
--- server/test/setup.js
+++ server/test/setup.js
@@ -2,20 +2,38 @@ const mongoose = require('mongoose');
 const { connect } = require('../index');
 const config = require('../config');
 
-const clearDb = (done) => {
-  mongoose.connection.dropDatabase();
-  return done();
+const clearDb = async () => {
+  await mongoose.connection.dropDatabase();
+  //return done();
 };
 
-beforeEach(async (done) => {
+beforeAll(async () => {
   if (mongoose.connection.readyState === 0) {
     //await connect(`${config.db.test}-${process.env.DB_URL_TEST}`);
     await connect(config.db.test);
   }
-  return clearDb(done);
+
+  await mongoose.connection.dropDatabase();
 });
 
-afterEach(async (done) => {
+beforeEach(async () => {
+  if (mongoose.connection.readyState === 0) {
+    //await connect(`${config.db.test}-${process.env.DB_URL_TEST}`);
+    await connect(config.db.test);
+  }
+  //return clearDb(done);
+  // return done();
+});
+
+afterAll(async () => {
   await mongoose.connection.close();
+  // return done();
+});
+
+/*afterEach(async (done) => {
+//  await mongoose.connection.close();
   return done();
 });
+*/
+
+
